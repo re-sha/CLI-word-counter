7255730a750801f66ea46634ada29a8759a6bee9
re-sha <163538829+re-sha@users.noreply.github.com>
2024-09-02 04:05:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('count-words')
+  .description('CLI to count number of words in file')
+  .version('1.0.0');
+
+program
+  .argument('<file>', 'file to count the number of words in it')
+  .action((file) => {
+    fs.readFile(file,"utf-8",(err,data) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        const words = data.trim().split(/\s+/);
+        console.log(`Number of words inside this file is ${words.length}`)
+    })
+  });
+
+program.parse();
